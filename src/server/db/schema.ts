81200e6a1db9270284b39a7478fda2aa56047a1b
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2024-11-13 15:09:07 -0500

    updated schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,18 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import {
+  type InferInsertModel,
+  type InferSelectModel,
+  relations,
+} from "drizzle-orm";
 import {
   index,
   integer,
   pgTableCreator,
   timestamp,
   varchar,
+  pgTable,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,19 +23,57 @@ import {
  */
 export const createTable = pgTableCreator((name) => `uno_${name}`);
 
-export const posts = createTable(
-  "post",
+export const user = pgTable(
+  "user",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
     name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
+    email: varchar("email", { length: 256 }).unique(),
+    password: varchar("password", { length: 256 }),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (table) => {
+    return {
+      indexOnEmail: index("user_email_index").on(table.email),
+      indexOnPassword: index("user_password_index").on(table.password),
+    };
+  },
 );
+
+export const userRelations = relations(user, ({ many }) => ({
+  sessions: many(session),
+}));
+
+export type CreateUser = InferInsertModel<typeof user>;
+export type User = InferSelectModel<typeof user>;
+export type UpdateUser = Partial<CreateUser>;
+
+export const session = pgTable(
+  "session",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    userId: integer("user_id")
+      .references(() => user.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      })
+      .notNull(),
+    token: varchar("token", { length: 250 }).notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
+  },
+  (table) => {
+    return {
+      indexOnToken: index("session_token_index").on(table.token),
+    };
+  },
+);
+
+export const sessionRelations = relations(session, ({ one }) => ({
+  user: one(user, {
+    fields: [session.userId],
+    references: [user.id],
+  }),
+}));
+
+export type CreateSession = InferInsertModel<typeof session>;
+export type Session = InferSelectModel<typeof session>;
+export type UpdateSession = Partial<CreateSession>;
